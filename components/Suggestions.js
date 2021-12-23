1f8e8903fecfb98ff9ba9df5add9b96c1015d984
Frank Mckenna <fmckenna56330@gmail.com>
2021-12-23 10:13:45 +0000

    feat: add suggestions for users to follow

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,7 +1,40 @@
+import faker from "faker";
+import { useEffect, useState } from "react";
+
 function Suggestions() {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => (
+      {
+        ...faker.helpers.contextualCard(),
+        id: i
+      }
+    ));
+    setSuggestions(suggestions);
+  }, [])
+
   return (
-    <div>
-      
+    <div className="mt-4 ml-10">
+      <div className="flex justify-between text-sm mb-5">
+        <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
+        <button className="text-gray-600 font-semibold">See All</button>
+      </div>
+      {suggestions.map(profile => {
+        <div
+        key={profile.id}
+        className='w-10 h-10 rounded-full border p-[2px]'
+        src={profile.avatar}
+        >
+
+          <div className="flex-1 ml-4">
+            <h2 className="font-semibold text-sm">{profile.username}</h2>
+            <h3 className="text text-gray-400">Works at {profile.company.name}</h3>
+          </div>
+
+          <button className="text-blue-400 text-xs font-bold" >Follow</button>
+        </div>
+      })}
     </div>
   )
 }
