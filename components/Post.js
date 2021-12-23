3a20e225c4f167a4b9139d6cf1241c714ed9652b
Frank Mckenna <fmckenna56330@gmail.com>
2021-12-23 08:21:32 +0000

    feat: add username and caption to post footer

--- components/Post.js
+++ components/Post.js
@@ -41,6 +41,12 @@ function Post({ id, username, userImg, caption, img }) {
         <BookmarkAltIcon className='btn' />
       </div>
 
+      {/* caption */}
+      <p className='p-5 truncate'>
+        <span className='font-bold mr-1'>{username}</span>
+        {caption}
+      </p>
+
       {/* comments */}
 
       {/* input box */}
