59c69a822f14768ee91b0dc77612334bf47ebce7
Frank Mckenna <fmckenna56330@gmail.com>
2021-12-27 20:24:31 +0000

    feat: when user is signed out center feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,12 +1,14 @@
+import { useSession } from "next-auth/react";
 import MiniProfile from "./MiniProfile"
 import Posts from "./Posts"
 import Stories from "./Stories"
 import Suggestions from "./Suggestions"
 
 function Feed() {
+  const {data: session } = useSession();
   return (
-    <main className='grid grid-cols-1 md:grid-cols2 md:max-w-3xl 
-    xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+    <main className={`grid grid-cols-1 md:grid-cols2 md:max-w-3xl 
+    xl:grid-cols-3 xl:max-w-6xl mx-auto ${!session && '!grid-cols-1 !max-w-3xl' }`} > 
       <section className='col-span-2'>
         {/* section */}
         {/* Stories */}
@@ -14,16 +16,17 @@ function Feed() {
         <Posts />
       </section>
 
-
-      <section className='hidden xl:inline-grid md:col-span-1'>
-        {/* Mini profile */}
-        <div className="fixed top-20">
-          <MiniProfile />
-          <Suggestions />
-        </div>
-          
-        {/* Suggestions */}
-      </section>
+      {session && 
+        <section className='hidden xl:inline-grid md:col-span-1'>
+          {/* Mini profile */}
+          <div className="fixed top-20">
+            <MiniProfile />
+            <Suggestions />
+          </div>
+            
+          {/* Suggestions */}
+        </section>      
+      }
 
     </main>
   )
