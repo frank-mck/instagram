acb07d05d8a5c06975a1a5cb4539385b0b803f5e
Frank Mckenna <fmckenna56330@gmail.com>
2021-12-27 20:18:31 +0000

    feat: update miniProfile to render dynamic data

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -8,15 +8,18 @@ function MiniProfile() {
     <div className="flex items-center justify-between mt-14 ml-10">
       <img 
         className="w-16 h-16 rounded-full border p-[2px] mt-14 ml-10"
-        src ='https://user-images.githubusercontent.com/77457834/146947844-21dbf64d-a1f1-4c94-b98b-170cda67719d.png'
+        src ={session?.user?.image}
         alt='' />
         
         <div className="flex-1 mx-4 ">
-          <h2 className="font-bold">frank</h2>
+          <h2 className="font-bold">{session?.user?.username}</h2>
           <h3 className="text-sm">Welcome to Instagram</h3>
         </div>
 
-        <button className="text-blue-400 text-sm font-semibold">Sign out</button>
+        <button 
+        className="text-blue-400 text-sm font-semibold"
+        onClick={signOut}
+        >Sign out</button>
     </div>
   )
 }
