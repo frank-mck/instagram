780993f4bb9e5d34b59d34dbe6e75acd94b8703e
Frank Mckenna <fmckenna56330@gmail.com>
2022-01-07 14:25:20 +0000

    feat: complete modal functionality

--- components/Modal.js
+++ components/Modal.js
@@ -35,7 +35,22 @@ function Modal() {
                 &#8203;
               </span>
 
-              
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <div className='inline-block align-bottom bg-white rounded-lg px-4 pt-5
+                pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8
+                sm:align-middle sm:max-w-sm sm:w-full'>
+                  <h1>Hello</h1>
+                </div>
+
+              </Transition.Child>
         </div>
         </Dialog>
       </Transition.Root>
