53a283e198c436e695bbcdca82d7dff83e977fee
Frank Mckenna <fmckenna56330@gmail.com>
2022-01-07 14:42:57 +0000

    feat: add button to modal

--- components/Modal.js
+++ components/Modal.js
@@ -47,7 +47,21 @@ function Modal() {
                 <div className='inline-block align-bottom bg-white rounded-lg px-4 pt-5
                 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8
                 sm:align-middle sm:max-w-sm sm:w-full'>
-                  <h1>Hello</h1>
+                  <div>
+                    <div className='mt-5 sm:mt-6'>
+
+
+                      <button
+                        type='button'
+                        className="inline-flex justify-center w-full rounded-md border border-transparent
+                        shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700
+                        focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm
+                        disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                      >  
+                      Upload Post                      
+                      </button>
+                    </div>
+                  </div>
                 </div>
 
               </Transition.Child>
